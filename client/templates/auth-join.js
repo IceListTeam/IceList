06f10d74c887a844be6f82f3e49972f29d8cb7ef
Bob Carney <rcarney1873@gmail.com>
2016-02-08 00:26:16 -0500

    added fields to join template

--- client/templates/auth-join.js
+++ client/templates/auth-join.js
@@ -4,6 +4,33 @@ Template.join.onCreated(function() {
   Session.set(ERRORS_KEY, {});
 });
 
+
+/*
+Template.join.onRendered(function() {
+  $('.input-symbol').validate({
+    rules: {
+      email: {
+        required:true,
+        email:true
+      },
+      password: {
+        required: true,
+        minlength: 8
+      }
+    },
+    messages: {
+      email: {
+        required: "Please enter your email address."
+        email: "Please enter a valid email address."
+      },
+      password: {
+        required: "Please enter your password"
+      }
+    }
+  });
+});
+*/
+
 Template.join.helpers({
   errorMessages: function() {
     return _.values(Session.get(ERRORS_KEY));
@@ -17,6 +44,12 @@ Template.join.events({
   'submit': function(event, template) {
     event.preventDefault();
     var email = template.$('[name=email]').val();
+    var firstName = template.$('[name=FirstName]').val();
+    var lastName = template.$('[name=LastName]').val();
+    var birthday = template.$('[name=Birthday]').val();
+    var major = template.$('[name=Major]').val();
+    var gradDate = template.$('[name=GradDate]').val();
+    var phone = template.$('[name=Phone]').val();
     var password = template.$('[name=password]').val();
     var confirm = template.$('[name=confirm]').val();
 
@@ -41,6 +74,12 @@ Template.join.events({
 
     Accounts.createUser({
       email: email,
+      firstname: firstName,
+      lastname: lastName,
+      birthday: birthday,
+      major: major,
+      gradDate: gradDate,
+      phone: phone,
       password: password
     }, function(error) {
       if (error) {
---
+++ server/smtp.js
@@ -0,0 +1,14 @@
+Meteor.startup(function () {
+process.env.MAIL_URL = 'smtp://icelist:P@ssw0rd1@smtp.sendgrid.net:587';
+});
+
+/*In your Meteor code place the following where you want to send the email(s).
+if (Meteor.isServer) {
+Email.send({
+from: "from@mailinator.com",
+to: "alpha@mailinator.com",
+subject: "Subject",
+text: "Here is some text"
+});
+}
+*/
