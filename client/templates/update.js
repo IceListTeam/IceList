c6f0c5de1a8f3eaa0f89ee6106d50f0335a01255
Bob Carney <bcarney@problemsolutions.net>
2016-02-16 17:51:02 -0500

    added Derek's changes

---
+++ client/templates/update.js
@@ -0,0 +1,73 @@
+var ERRORS_KEY = 'updateErrors';
+
+
+Template.update.onCreated(function() {
+  Session.set(ERRORS_KEY, {});
+});
+
+
+
+
+Template.update.onRendered(function() {
+  $('.input-symbol').validate();
+  console.log("hey im in onRendered");
+ 
+
+
+});
+ 
+
+Template.update.helpers({
+  userHelp: function(varn) {
+	return Meteor.user().profile[varn];
+  },
+  errorMessages: function() {
+    return _.values(Session.get(ERRORS_KEY));
+  },
+  errorClass: function(key) {
+    return Session.get(ERRORS_KEY)[key] && 'error';
+  }
+  
+  
+  
+
+
+});
+
+Template.update.events({
+  'submit': function(event, template) {
+    event.preventDefault();
+    var email = template.$('[name=email]').val();
+    var firstName = template.$('[name=FirstName]').val();
+    var lastName = template.$('[name=LastName]').val();
+    var birthday = template.$('[name=Birthday]').val();
+    var major = template.$('[name=Major]').val();
+    var gradDate = template.$('[name=GradDate]').val();
+    var phone = template.$('[name=Phone]').val();
+    var inPass = template.$('[name=password]').val();
+    var confPass = template.$('[name=confirm]').val();
+
+    var errors = {};
+    var user;
+
+
+
+    Session.set(ERRORS_KEY, errors);
+    if (_.keys(errors).length) {
+      return;
+    }
+
+    user = {
+      firstname: firstName,
+      lastname: lastName,
+      birthday: birthday,
+      major: major,
+      gradDate: gradDate,
+      phone: phone,
+      email:email
+    }
+
+	Meteor.users.update( { _id: Meteor.user()._id }, { $set: { "profile.firstname": firstName, "profile.lastname": lastName, "profile.name": firstName + lastName, "profile.birthday": birthday, "profile.major": major, "profile.gradDate": gradDate, "profile.phone": phone, "profile.email": email }});
+      Router.go('home');
+	}
+});
