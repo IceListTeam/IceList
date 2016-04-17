09552dace8913982c7281b82c91f2c80dff64923
Holden Jones <pmrcustomerservice@gmail.com>
2016-04-17 16:29:53 -0400

    Solved image upload URL display issue for Bob + Derek

--- client/templates/upload.js
+++ client/templates/upload.js
@@ -4,31 +4,31 @@ var imagelocation = ""
 
 Template.upload.events({'change .uploadFile': function(event, template) {
 
-             event.preventDefault();
+  event.preventDefault();
 
-             var upload1 = new Slingshot.Upload("myImageUploads");
-             var timeStamp = Math.floor(Date.now());                 
-         upload1.send(document.getElementById('uploadFile').files[0], function (error, downloadUrl) {
-             uploader.set();
-             if (error) {
-               console.error('Error uploading');
-               alert (error);
-             }
-             else{
-               console.log("Success!");
-               console.log('uploaded file available here: '+downloadUrl);
-               imagelocation = downloadUrl;
-               imageDetails.insert({
-                   imageurl: downloadUrl,
-                   time: timeStamp,
-                   uploadedBy: currentUserId
-               });
-             }
-             });
-             uploader.set(upload1);
-           }
-       });
-	   
+  var upload1 = new Slingshot.Upload("myImageUploads");
+  var timeStamp = Math.floor(Date.now());                 
+  upload1.send(document.getElementById('uploadFile').files[0], function (error, downloadUrl) {
+    uploader.set();
+    if (error) {
+      console.error('Error uploading');
+      alert (error);
+    }
+    else{
+      console.log("Success!");
+      console.log('uploaded file available here: '+downloadUrl);
+      imageDetails.insert({
+        imageurl: downloadUrl,
+        time: timeStamp,
+        uploadedBy: currentUserId
+      });
+      template.$("[id=imgloc]").html(downloadUrl);
+    }
+  });
+  uploader.set(upload1);
+  }
+});
+
 Template.upload.helpers({
 
     isUploading: function () {
@@ -39,14 +39,6 @@ Template.upload.helpers({
     var upload2 = uploader.get();
     if (upload2)
     return Math.round(upload2.progress() * 100);
-    },
-
-    url: function () {
-
-    //console.log( imageDetails.findOne({uploadedBy: currentUserId},{sort:{ time : -1 } }) );
-    
-     return("the image location is: " + imagelocation);
-
-    },
+    }
 
 });
